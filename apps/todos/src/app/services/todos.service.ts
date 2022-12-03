9d1673cde356ec57ded4e37292cba508191d5e99
Fabio <fabioiglesias@hotmail.es>
2022-12-03 16:56:17 +0100

    feat(todos-service): find all

--- apps/todos/src/app/services/todos.service.ts
+++ apps/todos/src/app/services/todos.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AllFoundTodos } from '../models/todo.model';
 
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
 @Injectable({
   providedIn: 'root',
 })
 export class TodosService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  findAll(): Observable<AllFoundTodos[]> {
-    throw 'Not implemented';
+  findAll(): Observable<AllFoundTodos> {
+    return this.http.get<AllFoundTodos>(`${environment.apiRoot}/todos`);
   }
 }
